9f1c2d5251d751f457ea967fc1be0426e5b73224
Daniel <danielbellinijf21@gmail.com>
2023-01-20 17:35:38 -0300

    feat: implement route /enrroument/cep

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,10 +1,19 @@
 import axios from "axios";
 import { requestError } from "@/errors";
+import { ViaCEPAddress } from "@/protocols";
 
 async function get(url: string) {
   try {
     const result = await axios.get(url);
-    return result;
+
+    const mock: ViaCEPAddress = {
+      logradouro: result.data.logradouro,
+      complemento: result.data.complemento,
+      bairro: result.data.bairro,
+      localidade: result.data.localidade,
+      uf: result.data.uf,
+    }
+    return mock;
   } catch (error) {
     const {
       status,
